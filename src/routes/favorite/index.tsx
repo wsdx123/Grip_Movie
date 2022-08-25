a25e738072a55945b9e27fdfd6ae367f7695a598
Jun Young <50202150+wsdx123@users.noreply.github.com>
2022-08-25 20:28:58 +0900

    fix: favorite 컴포넌트의 일반state 제거

--- src/routes/favorite/index.tsx
+++ src/routes/favorite/index.tsx
@@ -1,22 +1,15 @@
 import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd'
-import { useEffect, useState } from 'react'
 import { useMount } from 'react-use'
 import store from 'storejs'
 
 import { favoriteDataState } from 'states/movie'
-import { IFavoriteArr } from 'types/home.d'
 import { useRecoil } from 'hooks/state'
 
 import MovieItem from 'components/movieItem'
 import styles from './favorite.module.scss'
 
 const Favorite = () => {
-  const [ifavData, setIFavData] = useState<IFavoriteArr>([])
   const [favData, setFavData] = useRecoil(favoriteDataState)
-
-  useEffect(() => {
-    setIFavData(favData)
-  }, [favData])
 
   useMount(() => {
     if (!store.has('favorite')) return
@@ -28,24 +21,23 @@ const Favorite = () => {
 
     if (!destination) return
 
-    const items = Array.from(ifavData)
+    const items = Array.from(favData)
     const [reorderData] = items.splice(source.index, 1)
     items.splice(destination.index, 0, reorderData)
 
-    setIFavData(items)
+    setFavData(items)
     store.set('favorite', items)
-    setFavData(store.get('favorite'))
   }
 
   return (
     <div className={styles.container}>
       <div>
-        {ifavData.length === 0 && <div>즐겨찾기를 추가해주세요.</div>}
+        {favData.length === 0 && <div>즐겨찾기를 추가해주세요.</div>}
         <DragDropContext onDragEnd={onDragEnd}>
           <Droppable droppableId='favorite-dnd'>
             {(provided) => (
               <ul ref={provided.innerRef} {...provided.droppableProps} className={styles.movielist}>
-                {ifavData.map((el, index) => {
+                {favData.map((el, index) => {
                   return (
                     <Draggable key={el.imdbID} draggableId={el.imdbID} index={index}>
                       {(innerprovided) => <MovieItem Item={el} innerProvided={innerprovided} />}
